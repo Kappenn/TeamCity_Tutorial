7b38a7b7025a08befa32b75c75c1fbd34aaa3756
Kappenn <33659766+Kappenn@users.noreply.github.com>
2020-04-01 17:05:34 +0200

    Update AppTest.java

--- src/test/java/Tutorial/demo/AppTest.java
+++ src/test/java/Tutorial/demo/AppTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void https://www.youtube.com/watch?v=dQw4w9WgXcQ() {
+    @Test public void RememberToWashYourHandsForAtLeast20Seconds {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
